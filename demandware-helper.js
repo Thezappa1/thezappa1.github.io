542dd45b70cbc2ee95aa169622da2c0e23be8719
Helen <helen@Helens-MacBook-Air.local>
2015-02-27 13:04:00 -0800

    adding yahoo API test external call

--- demandware-helper.js
+++ demandware-helper.js
@@ -1,21 +1,26 @@
 var Hello = function() {
-  alert("Hello!")
+
+  var xhr = new XMLHttpRequest
+  xhr.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+  xhr.send();
+  alert(xhr.responseText().query.results.channel.wind);
+
 }
 
 var demandwareRestApi = function (method,url,params) {
 			
+
         var myHttp = function(method, theUrl) {
           var xmlHttp = null;
           xmlHttp = new XMLHttpRequest();
           xmlHttp.open(method, theUrl, false );
           xmlHttp.send( null );
          return xmlHttp.responseText;
-        } 
+        }; 
 
-
-			  var baseUrl = "https://3tierlogic01-tech-prtnr-na01-dw.demandware.net/s/SiteGenesis/dw/shop/"
-				var apiVersion = "v15_2"
-				var clientId = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+			  var baseUrl = "https://3tierlogic01-tech-prtnr-na01-dw.demandware.net/s/SiteGenesis/dw/shop/";
+				var apiVersion = "v15_2";
+				var clientId = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 				
         		var callUrl = baseUrl + '/' + apiVersion + '/' + url + '?';
         		var urlParams = [];
