151e5de716aa0a3c0836d48f49653cf38f726435
Thezappa1 <helenauerbach@gmail.com>
2015-03-04 15:13:04 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
 jQuery(document).ready(function($){
-			//open popup
-			jQuery("a[class='cd-popup-trigger']").on('click', function(event){
-				event.preventDefault();
-				$('.cd-popup').addClass('is-visible');
-			});
-			
-			//close popup
-			jQuery("a[class='cd-popup']").on('click', function(event){
-				if( $(event.target).is('a[class='cd-popup-close']') || $(event.target).is('a[class='cd-popup']') ) {
-					event.preventDefault();
-					$(this).removeClass('is-visible');
-				}
-			});
-			//close popup when clicking the esc keyboard button
-			$(document).keyup(function(event){
-		    	if(event.which=='27'){
-		    		jQuery("a[class='cd-popup']").removeClass('is-visible');
-			    }
-		    });
-		});
+	//open popup
+	jQuery("a[class='cd-popup-trigger']").on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	jQuery("a[class='cd-popup']").on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
